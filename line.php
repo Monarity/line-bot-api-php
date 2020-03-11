db53c4b47c28e40de06f4967a79a65dafe125818
Monarity <62059830+Monarity@users.noreply.github.com>
2020-03-11 19:46:42 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'BvDsy8t/wkYCho4aIR/uK1JJquwnnTI7vnenWiu3Df6TEvLoBptYR7HonAtmHUKDQOffAkWetc4iw4Mkw7u4rlMM/VsBB6A5D7DfZ3NjO9kwWKC6wiHm3vvk86eBVCKUyT1nwxoYwLl0ok5n1JxrhQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = '/vmWQWplFAnRc1zDxDKcVldJwLj/XBJntJzwYyVdB1wu7ng1SeBrSLX5GblMG2MWI8Qez7Yxhz4jq7lMa6Emq1CGWszILLANOOealsS4ftFplTLVJnLW+6fNnkRJF8qQH/hDQVdB9Ro7bvha7AFPWgdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
